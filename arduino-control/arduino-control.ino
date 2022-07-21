04a7c00709d5e1b369caad9bfb11fc554a924e22
adriahf <adriahuguet@gmail.com>
2022-07-21 23:00:31 +0200

    get time in a cleaner way

--- arduino-control/arduino-control.ino
+++ arduino-control/arduino-control.ino
@@ -1,28 +1,56 @@
-/*
-  Plant TV Arduino Corntrol
+#include <Process.h>
 
-  This sketch controls all the operating logic of the Plant TV project. 
- */
+// process used to get the date
+Process date;
 
-#include <Bridge.h>
-#include <HttpClient.h>
-#include <ArduinoJson.h>
+// Python equivalent split function
+String split(String data, char separator, int index) {
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+// get time function, given the time in a String format
+void get_time(String data) {
+  int year = split(data, ':', 0).toInt();
+  int month = split(data, ':', 1).toInt();
+  int day = split(data, ':', 2).toInt();
+  int hour = split(data, ':', 3).toInt();
+  int minute = split(data, ':', 4).toInt();
+  int second = split(data, ':', 5).toInt();
+}
 
 void setup() {
-  SerialUSB.begin(9600);
-  // wait for a serial connection
-  while (!SerialUSB); 
+  // initialize Bridge
+  Bridge.begin();
+  // initialize serial 
+  Serial.begin(9600); 
+  // wait for Serial Monitor to open
+  while(!Serial);
+  Serial.println("Initializing...");
 }
 
 void loop() {
-  String current_date_time = get_time_string();
-  int year = current_date_time.substring(0, 4).toInt();
-  int month = current_date_time.substring(5, 7).toInt();
-  int day = current_date_time.substring(8, 10).toInt();
-  int hour = current_date_time.substring(11, 13).toInt();
-  int minute = current_date_time.substring(14, 16).toInt();
+  // begin date
+  date.begin("/bin/date");
+  // time format
+  date.addParameter("+%Y:%m:%d:%H:%M:%S");
+  date.run();
   
-  SerialUSB.flush();
-
-  delay(5000);
+  // if there's a result from the date process, get it.
+  while (date.available() > 0) {
+    // get the time
+    get_time(date.readString());
+  }
+  
+  delay(5000); // wait 5000ms
 }
